2ac55f0cbc0c2e8835c4a42f0a43a0f735bec812
Rémi Defoor <remi.defoor@student.howest.be>
2022-04-29 16:11:46 +0200

    register db context as service

--- Howest.Magic.WebAPI/Program.cs
+++ Howest.Magic.WebAPI/Program.cs
@@ -1,4 +1,7 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+ConfigurationManager config = builder.Configuration;
 
 // Add services to the container.
 
@@ -6,8 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<mtg_v1Context>(options =>
+    options.UseSqlServer(config.GetConnectionString("MtgV1Db"));
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
