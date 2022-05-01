69683c5a0e92c90b8a0154c87ecdb9f18853e500
Rémi Defoor <remi.defoor@student.howest.be>
2022-05-01 08:17:08 +0200

    create card read dto

---
+++ Howest.MagicCards.Shared/DTOs/CardReadDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Howest.MagicCards.Shared.DTOs;
+
+public record CardReadDTO
+{
+    public long Id { get; init; }
+    public string Name { get; init; }
+    public string ManaCost { get; init; }
+    public string Power { get; init; }
+    public string Toughness { get; init; }
+    public string ImageUrl { get; init; }
+}
