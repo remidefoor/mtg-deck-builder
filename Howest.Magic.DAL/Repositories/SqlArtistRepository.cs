4acd29eb3536ae25491aee42e17b07e771b72a85
Rémi Defoor <remi.defoor@student.howest.be>
2022-05-04 10:50:46 +0200

    create SQL artist repository

---
+++ Howest.Magic.DAL/Repositories/SqlArtistRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace Howest.MagicCards.DAL.Repositories;
+
+public class SqlArtistRepository : IArtistRepository
+{
+    private readonly mtg_v1Context _db;
+
+    public SqlArtistRepository(mtg_v1Context mtg_V1DBContext)
+    {
+        _db = mtg_V1DBContext;
+    }
+
+    public IQueryable<Artist> GetArtists()
+    {
+        return _db.Artists
+            .Select(artist => artist);
+    }
+
+    public Artist GetArtist(long id)
+    {
+        return _db.Artists
+            .SingleOrDefault(artist => artist.Id == id);
+    }
+}
