f242c451e719284d2ad7ddc079fc847c8375ede1
Rémi Defoor <remi.defoor@student.howest.be>
2022-05-08 13:52:33 +0200

    create SQL deck repository

---
+++ Howest.Magic.DAL/Repositories/SqlDeckRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Howest.MagicCards.DAL.Repositories;
+public class SqlDeckRepository : IDeckRepository
+{
+    private readonly mtg_v1Context _db;
+
+    public SqlDeckRepository(mtg_v1Context mtg_V1DBContext)
+    {
+        _db = mtg_V1DBContext;
+    }
+
+    public void CreateDeck(List<long> cards)
+    {
+        // TODO implement
+    }
+
+    public void DeleteDeck()
+    {
+        // TODO implement
+    }
+}
