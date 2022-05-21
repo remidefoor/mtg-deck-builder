1fa1e39bfe611874fd52ec923b766557b1829c44
Rémi Defoor <remi.defoor@student.howest.be>
2022-05-21 09:00:25 +0200

    add required data anotation to deck card amount

--- Howest.MagicCards.Shared/DTOs/DeckCardWriteDTO.cs
+++ Howest.MagicCards.Shared/DTOs/DeckCardWriteDTO.cs
@@ -5,6 +5,7 @@ namespace Howest.MagicCards.Shared.DTOs;
 public record DeckCardWriteDTO
 {
     public long CardId { get; init; }
+    [Required(ErrorMessage = "Pleas provide an amount for the deck card")]
     [Range(1, 60)]
     public int Amount { get; init; } = 1;
 }
